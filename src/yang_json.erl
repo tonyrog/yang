c31238da9f7925748ad819ad54bcfdff801d5734
Ulf Wiger <ulf@feuerlabs.com>
2012-02-16 00:56:53 +0100

    handle split yang files in yang_json

--- src/yang_json.erl
+++ src/yang_json.erl
@@ -2,51 +2,78 @@
 -compile(export_all).
 
 json_rpc(YangFile) ->
+    Dir = filename:dirname(YangFile),
     case yang:parse_file(YangFile) of
 	{ok, Y} ->
-	    each_module(fun to_json_rpc/1, Y);
+	    each_module(fun to_json_rpc/2, Y, Dir);
 	Error ->
 	    Error
     end.
 
-each_module(F, [{module,_,M,Data}|Rest]) ->
-    [{module, binary_to_list(M), F(Data)} | each_module(F, Rest)];
-each_module(F, [_|Rest]) ->
-    each_module(F, Rest);
-each_module(_, []) ->
+each_module(F, [{module,_,M,Data}|Rest], Dir) ->
+    [{module, binary_to_list(M), F(Data, Dir)} | each_module(F, Rest, Dir)];
+each_module(F, [_|Rest], Dir) ->
+    each_module(F, Rest, Dir);
+each_module(_, [], _) ->
     [].
 
-to_json_rpc(Data) ->
-    [{binary_to_list(N), mk_rpc_pair(InOut, N, Data)} || {rpc,_,N,InOut} <- Data].
+to_json_rpc(Data, Dir) ->
+    Imports = imports(Data, Dir),
+    [{binary_to_list(N), mk_rpc_pair(InOut, N, Data, Imports)} || {rpc,_,N,InOut} <- Data].
 
-mk_rpc_pair(InOut, N, Data) ->
+imports(Data, Dir) ->
+    lists:foldl(
+      fun({import,_,F,[{prefix,_,Pfx,_}|_]}, Acc) ->
+	      case yang:parse_file(filename:join(Dir, binary_to_list(F) ++ ".yang")) of
+		  {ok, Y} ->
+		      case [D || {module,_,N,D} <- Y,
+				 N == F] of
+			  [ImpData] ->
+			      orddict:store(Pfx, ImpData, Acc);
+			  [] ->
+			      error({cannot_import, F, no_such_module})
+		      end;
+		  Error ->
+		      error({cannot_import, F, Error})
+	      end;
+	 (_, Acc) ->
+	      Acc
+      end, orddict:new(), Data).
+
+mk_rpc_pair(InOut, N, Data, Imports) ->
     {_,_,_,I} = lists:keyfind(input, 1, InOut),
     {_,_,_,O} = lists:keyfind(output, 1, InOut),
     {{request, {struct, [{"json-rpc", "2.0"},
 			 {"method", binary_to_list(N)},
 			 {"id", ""},
-			 {"params", {struct, rpc_params(I, Data)}}]}},
+			 {"params", {struct, rpc_params(I, Data, Imports)}}]}},
      {reply, {struct, [{"json-rpc", "2.0"},
 		       {"id", ""},
-		       {"result", {struct, rpc_params(O, Data)}}]}}}.
+		       {"result", {struct, rpc_params(O, Data, Imports)}}]}}}.
 
-rpc_params([{uses,_,G,_}|T], Data) ->
-    case [L || {grouping,_,Grp,L} <- Data,
-	       Grp == G] of
+rpc_params([{uses,_,G,_}|T], Data, Imports) ->
+    {Where, GrpName} = case re:split(G, <<":">>) of
+			   [_] ->
+			       {Data, G};
+			   [Pfx, G1] ->
+			       {orddict:fetch(Pfx, Imports), G1}
+		       end,
+    case [L || {grouping,_,Grp,L} <- Where,
+	       Grp == GrpName] of
 	[] ->
-	    [{"uses-not-found", binary_to_list(G)} | rpc_params(T, Data)];
+	    [{"uses-not-found", binary_to_list(G)} | rpc_params(T, Data, Imports)];
 	[Params] ->
-	    rpc_params(Params, Data) ++ rpc_params(T, Data)
+	    rpc_params(Params, Data, Imports) ++ rpc_params(T, Data, Imports)
     end;
-rpc_params([{leaf,_,N,_}|T], Data) ->
-    [{binary_to_list(N), ""} | rpc_params(T, Data)];
-rpc_params([{anyxml,_,N,_}|T], Data) ->
-    [{binary_to_list(N), ""} | rpc_params(T, Data)];
-rpc_params([{list,_,N,Items}|T], Data) ->
-    [{binary_to_list(N), {array, [{struct, rpc_params(Items, Data)}]}} | rpc_params(T, Data)];
-rpc_params([_|T], Data) ->
-    rpc_params(T, Data);
-rpc_params([], _) ->
+rpc_params([{leaf,_,N,_}|T], Data, Imports) ->
+    [{binary_to_list(N), ""} | rpc_params(T, Data, Imports)];
+rpc_params([{anyxml,_,N,_}|T], Data, Imports) ->
+    [{binary_to_list(N), ""} | rpc_params(T, Data, Imports)];
+rpc_params([{list,_,N,Items}|T], Data, Is) ->
+    [{binary_to_list(N), {array, rpc_params(Items, Data, Is)}} | rpc_params(T, Data, Is)];
+rpc_params([_|T], Data, Is) ->
+    rpc_params(T, Data, Is);
+rpc_params([], _, _) ->
     [].
 
 markdown(File, JSON) ->
